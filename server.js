dfc0482b711807bdc1c9767c7873d4003adae935
Diaa-E <blarb21@yahoo.com>
2024-05-07 00:59:58 +0300

    Create simple router

--- server.js
+++ server.js
@@ -5,10 +5,27 @@ const HOSTNAME = "127.0.0.1";
 
 const server = createServer((req, res) => {
 
-    // res.statusCode = 200;
-    // res.setHeader("Content-Type", "text/html");
-    res.writeHead(200, { "Content-Type": "text/html" })
-    res.end("<h1>hello world</h1>");
+    switch (req.url)
+    {
+        case "/": 
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end("<h1>Home</h1>");
+            break;
+
+        case "/about": 
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end("<h1>About</h1>");
+            break;
+
+        case "/contact": 
+        res.writeHead(200, { "Content-Type": "text/html" });
+            res.end("<h1>Contact</h1>");
+            break;
+
+        default: 
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end("<h1>Error 404</h1>");
+    }
 });
 
 server.listen(PORT, HOSTNAME, () => console.log(`server running at http://${HOSTNAME}:${PORT}/`));
