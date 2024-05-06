69dff5e81c7217c6e5959904652d1c6ec864f5fa
Diaa-E <blarb21@yahoo.com>
2024-05-06 23:42:40 +0300

    Create server using http module

---
+++ server.js
@@ -0,0 +1,14 @@
+import { createServer } from "node:http";
+
+const PORT = 5000;
+const HOSTNAME = "127.0.0.1";
+
+const server = createServer((req, res) => {
+
+    // res.statusCode = 200;
+    // res.setHeader("Content-Type", "text/html");
+    res.writeHead(200, { "Content-Type": "text/html" })
+    res.end("<h1>hello world</h1>");
+});
+
+server.listen(PORT, HOSTNAME, () => console.log(`server running at http://${HOSTNAME}:${PORT}/`));
